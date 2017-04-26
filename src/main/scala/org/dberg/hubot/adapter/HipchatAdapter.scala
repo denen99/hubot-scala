e20fc38dc05fd12434a045a90a1d7b52670528f5
denen99 <adam@dberg.org>
2017-04-26 15:52:27 -0400

    dont modify in the iterator

--- src/main/scala/org/dberg/hubot/adapter/HipchatAdapter.scala
+++ src/main/scala/org/dberg/hubot/adapter/HipchatAdapter.scala
@@ -164,8 +164,8 @@ class HipchatAdapter(hubot: Hubot) extends BaseAdapter(hubot: Hubot) with Strict
 
   import HipchatAdapterTools._
 
-  private def leaveJoinedRooms() = {
-    mucMgr.getJoinedRooms.asScala.foreach { room =>
+  private def leaveJoinedRooms(rooms: List[String]) = {
+    rooms.foreach { room =>
       logger.info("Forcing leave of room " + room)
       val m = mucMgr.getMultiUserChat(room)
       try {
@@ -192,7 +192,8 @@ class HipchatAdapter(hubot: Hubot) extends BaseAdapter(hubot: Hubot) with Strict
 
       pingMgr.pingMyServer(true) // Just a test
 
-      leaveJoinedRooms() // First leave the rooms
+      leaveJoinedRooms(mucMgr.getJoinedRooms.asScala.toList) // First leave the rooms
+
 
       mucMgr.getHostedRooms("conf.hipchat.com").asScala.foreach { room =>
         val muc = mucMgr.getMultiUserChat(room.getJid)
